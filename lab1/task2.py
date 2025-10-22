ace84b52bba3827eae3b4a514dc2774664d0fc76
Thrillslk <andrewshkarin.shkarin@yandex.ru>
2025-10-22 14:40:56 +0500

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,7 @@
-
+from math import cos, log
+a=((x^3)/2)^(1/2)-math.sin(y)
+b=(e^2)/(3)-math.cos(y)+z+math.ln(y)
+    x = float (input("Введите значение переменной x: "))
+    y = float (input("Введите значение переменной y: "))
+    z = float (input("Введите значение переменной z: "))
+print(f"Получено значение функции a={a}")
